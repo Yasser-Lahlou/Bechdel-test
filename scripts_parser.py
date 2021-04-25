72de4932291dc224331ce8ad8e1450597fe09291
Yasser-Lahlou <yasser.lahlougmail.com>
2021-04-25 21:57:02 +0200

    First commit

---
+++ scripts_parser.py
@@ -0,0 +1,50 @@
+import requests, re
+from bs4 import BeautifulSoup
+
+URL = 'https://imsdb.com/all-scripts.html'
+URL_BASE = 'https://www.imsdb.com/scripts/'
+
+headers = {
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 11_2_0) \
+            AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.146 Safari/537.36'
+            }
+# save all home page scripts
+def scrap_list(url):
+    response = requests.get(url, headers=headers)
+    with open('IMSDB_script_list', "w") as f:
+        f.write(response.text)
+scrap_list(URL)
+
+# create list of movie titles and script urls
+def movie_list():
+    with open('IMSDB_script_list', "r") as f:
+        data = f.read()
+    soup = BeautifulSoup(data, features="lxml")
+    movies_list = soup.select('p a')
+    list_urls = []
+    for movie in movies_list:
+        movie_title = movie['title']
+        movie_title = movie_title.replace(' ', '-')
+        movie_title = re.sub(r'\:', '', movie_title)
+        movie_title = re.sub(r'-Script', '', movie_title)
+        movie_url = URL_BASE + movie_title + '.html'
+        list_urls.append([movie_title, movie_url])
+    return list_urls
+
+# for each movie url, save script content in a .txt file (and identifies errors)
+def scrap_script(title, url):
+    response = requests.get(url, headers=headers)
+    soup = BeautifulSoup(response.text, features="lxml")
+    tags = soup.select('pre')
+    non_scraped = []
+    if len(tags) != 0:
+        script = tags[-1]
+        with open(f'IMSDB_scripts/{title}.txt', "w", encoding='utf-8', errors='ignore') as f:
+            f.write(script.text)
+    else:
+        non_scraped.append(title)
+    return non_scraped
+
+# create all .txt files
+for movie in movie_list():
+    scrap_script(movie[0], movie[1])
